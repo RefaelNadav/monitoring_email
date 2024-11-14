042f6e478a11a158ec636622706d838c5fce8b97
RefaelNadav <rn3857604@gmail.com>
2024-11-14 13:54:19 +0200

    processor stream added

---
+++ stream_processor/processor.py
@@ -0,0 +1,47 @@
+from kafka import KafkaConsumer, KafkaProducer
+import json
+
+# Kafka consumer and producer setup
+consumer = KafkaConsumer(
+    'messages.all',
+    bootstrap_servers=['localhost:9092'],
+    auto_offset_reset='earliest',
+    group_id='all_messages_processor_group',
+    value_deserializer=lambda m: json.loads(m.decode('utf-8'))
+)
+
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:9092'],
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+def is_hostage(email):
+    sentences = email.get('sentences')
+    for sentence in sentences:
+        if 'hostage' in sentence.lower():
+            return True
+    return False
+
+def is_explosive(email):
+    sentences = email.get('sentences')
+    for sentence in sentences:
+        if 'explos' in sentence.lower():
+            return True
+    return False
+
+for message in consumer:
+    try:
+        email = message.value
+    except Exception as e:
+        print("Deserialization error:", e)
+
+    print("Received message:", message)
+    email = message.value
+    print("Decoded message:", email)
+    # producer.send('all_messages', email)
+    if is_hostage(email):
+        producer.send('messages.hostage', value=email)
+        print(f"Suspicious content message: {email}")
+    elif is_explosive(email):
+        producer.send('messages.explosive', value=email)
+        print(f"Suspicious content message: {email}")
